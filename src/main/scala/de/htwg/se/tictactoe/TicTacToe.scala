0c708a146894781733e1d2ea31ba72e5e7dc8648
Marko Boger <marko.boger@gmail.com>
2021-11-01 17:52:32 +0100

    read and print loop

--- src/main/scala/de/htwg/se/tictactoe/TicTacToe.scala
+++ src/main/scala/de/htwg/se/tictactoe/TicTacToe.scala
@@ -4,10 +4,29 @@ import de.htwg.se.tictactoe.model.Stone
 import de.htwg.se.tictactoe.model.Matrix
 import de.htwg.se.tictactoe.model.Field
 
+import scala.io.StdIn.readLine
+
 @main def run: Unit =
   println("Welcome to TicTacToe")
   var field = new Field(3, Stone.Empty)
   println(field.toString)
-  field = field.put(Stone.X, 0, 0)
-  field = field.put(Stone.O, 1, 1)
-  println(field.toString)
+  getInputAndPrintLoop(field)
+
+def getInputAndPrintLoop(field: Field): Unit =
+  val input = readLine
+  input match
+    case "q" =>
+    case _ => {
+      val chars = input.toCharArray
+      val stone = chars(0) match
+        case 'X' => Stone.X
+        case 'x' => Stone.X
+        case 'O' => Stone.O
+        case 'o' => Stone.O
+        case _   => Stone.Empty
+      val x = chars(1).toString.toInt
+      val y = chars(2).toString.toInt
+      val newfield = field.put(stone, x, y)
+      println(newfield)
+      getInputAndPrintLoop(newfield)
+    }
