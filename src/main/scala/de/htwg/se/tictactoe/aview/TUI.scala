a4088f3d11e01dc1b23a123eaa3a3342349692d4
mboger <marko.boger@htwg-konstanz.de>
2021-11-05 11:38:43 +0100

    implemented observer

--- src/main/scala/de/htwg/se/tictactoe/aview/TUI.scala
+++ src/main/scala/de/htwg/se/tictactoe/aview/TUI.scala
@@ -4,27 +4,31 @@ package aview
 import controller.Controller
 import model.Stone
 import scala.io.StdIn.readLine
+import util.Observer
 
-class TUI(controller: Controller):
+class TUI(controller: Controller) extends Observer:
+  controller.add(this)
   def run =
     println(controller.field.toString)
-    getInputAndPrintLoop(controller)
+    getInputAndPrintLoop()
 
-def getInputAndPrintLoop(controller: Controller): Unit =
-  val input = readLine
-  input match
-    case "q" =>
-    case _ => {
-      val chars = input.toCharArray
-      val stone = chars(0) match
-        case 'X' => Stone.X
-        case 'x' => Stone.X
-        case 'O' => Stone.O
-        case 'o' => Stone.O
-        case _   => Stone.Empty
-      val x = chars(1).toString.toInt
-      val y = chars(2).toString.toInt
-      val newController = controller.put(stone, x, y)
-      println(newController.toString)
-      getInputAndPrintLoop(newController)
-    }
+  override def update = ???
+
+  def getInputAndPrintLoop(): Unit =
+    val input = readLine
+    input match
+      case "q" =>
+      case _ => {
+        val chars = input.toCharArray
+        val stone = chars(0) match
+          case 'X' => Stone.X
+          case 'x' => Stone.X
+          case 'O' => Stone.O
+          case 'o' => Stone.O
+          case _   => Stone.Empty
+        val x = chars(1).toString.toInt
+        val y = chars(2).toString.toInt
+        controller.put(stone, x, y)
+        println(controller.toString)
+        getInputAndPrintLoop()
+      }
--- src/main/scala/de/htwg/se/tictactoe/controller/Controller.scala
+++ src/main/scala/de/htwg/se/tictactoe/controller/Controller.scala
@@ -3,7 +3,10 @@ package controller
 
 import model.Field
 import model.Stone
+import util.Observable
 
-case class Controller(field: Field):
-  def put(stone: Stone, x: Int, y: Int) = copy(field.put(stone, x, y))
+case class Controller(var field: Field) extends Observable:
+  def put(stone: Stone, x: Int, y: Int) =
+    field = field.put(stone, x, y)
+    notifyObservers
   override def toString = field.toString
