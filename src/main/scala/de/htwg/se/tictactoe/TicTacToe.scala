5c8b440be61a4a9115627554fb8b1a140e692ab7
Marko Boger <marko.boger@gmail.com>
2022-04-08 12:35:39 +0200

    separated out parse method

--- src/main/scala/de/htwg/se/tictactoe/TicTacToe.scala
+++ src/main/scala/de/htwg/se/tictactoe/TicTacToe.scala
@@ -14,19 +14,24 @@ import scala.io.StdIn.readLine
 
 def getInputAndPrintLoop(field: Field): Unit =
   val input = readLine
-  input match
-    case "q" =>
-    case _ => {
-      val chars = input.toCharArray
-      val stone = chars(0) match
-        case 'X' => Stone.X
-        case 'x' => Stone.X
-        case 'O' => Stone.O
-        case 'o' => Stone.O
-        case _   => Stone.Empty
-      val x = chars(1).toString.toInt
-      val y = chars(2).toString.toInt
-      val newfield = field.put(stone, x, y)
+  parseInput(input) match
+    case None => field
+    case Some(newfield) =>
       println(newfield)
       getInputAndPrintLoop(newfield)
-    }
+
+  def parseInput(input: String): Option[Field] =
+    input match
+      case "q" => None
+      case _ => {
+        val chars = input.toCharArray
+        val stone = chars(0) match
+          case 'X' => Stone.X
+          case 'x' => Stone.X
+          case 'O' => Stone.O
+          case 'o' => Stone.O
+          case _   => Stone.Empty
+        val x = chars(1).toString.toInt
+        val y = chars(2).toString.toInt
+        Some(field.put(stone, x, y))
+      }
