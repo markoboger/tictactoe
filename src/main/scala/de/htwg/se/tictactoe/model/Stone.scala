8dd5f56544e655e0fa31132312f718257f81c1af
mboger <marko.boger@htwg-konstanz.de>
2025-03-28 11:57:22 +0100

    changed back to X

--- src/main/scala/de/htwg/se/tictactoe/model/Stone.scala
+++ src/main/scala/de/htwg/se/tictactoe/model/Stone.scala
@@ -2,6 +2,6 @@ package de.htwg.se.tictactoe.model
 
 enum Stone(stringRepresentation: String):
   override def toString = stringRepresentation
-  case X extends Stone("Y")
+  case X extends Stone("X")
   case O extends Stone("O")
   case Empty extends Stone(" ")
